3564c7a8e6bbeb44438e4fa899843e25ae9a77f8
Aksel Bouri <Arch@archlinux.localdomain>
2025-01-15 10:07:32 +0100

    commit

--- src/main/Java/Events.java
+++ src/main/Java/Events.java
@@ -1,4 +1,17 @@
 class Events{
+    final String ANSI_RESET = "\033[m";
+    final String ANSI_MAGENTA = "\033[35m";
+    final String ANSI_ORANGE = "\033[38;2;255;165;0m";
+    final String ANSI_GREENKINDOF = "\033[38;5;42m";
+    final String ANSI_BROWN = "\033[38;2;139;69;19m";
+    final String ANSI_GRAY = "\033[38;2;128;128;128m";
+    final String ANSI_BLUE_LIGHT = "\033[38;2;173;216;230m";
+    final String ANSI_GREEN_LIGHT = "\033[38;2;144;238;144m";
+    final String ANSI_CYAN_LIGHT = "\033[38;2;224;255;255m";
+    final String ANSI_YELLOW_BRIGHT = "\033[38;2;255;255;0m";
+    final String ANSI_YELLOW =  "\033[38;5;11m";
+    final String ANSI_ORANGE_DARK = "\033[38;2;255;140;0m";
+    final String ANSI_RED_BRIGHT = "\033[38;2;255;0;0m";
 
     boolean[] entrepotPlein=new boolean[]{false};
     String entrepotPleinSTR="Les Entrepôts sont pleins !";
